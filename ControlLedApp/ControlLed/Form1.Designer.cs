80586d3a2fda5497a1a49790014e8628b3178f55
quannar178 <quannar178@gmail.com>
2019-11-24 15:19:03 +0700

    First commit Control LED

---
+++ ControlLedApp/ControlLed/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace ControlLed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.grSetting = new System.Windows.Forms.GroupBox();
+            this.btDisconnet = new System.Windows.Forms.Button();
+            this.btConnect = new System.Windows.Forms.Button();
+            this.cbStopBits = new System.Windows.Forms.ComboBox();
+            this.cbParity = new System.Windows.Forms.ComboBox();
+            this.cbDataSize = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbPortBaud = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbPortName = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grStatus = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btLed1Off = new System.Windows.Forms.Button();
+            this.btLed1On = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.grSetting.SuspendLayout();
+            this.grStatus.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grSetting
+            // 
+            this.grSetting.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.grSetting.Controls.Add(this.btDisconnet);
+            this.grSetting.Controls.Add(this.btConnect);
+            this.grSetting.Controls.Add(this.cbStopBits);
+            this.grSetting.Controls.Add(this.cbParity);
+            this.grSetting.Controls.Add(this.cbDataSize);
+            this.grSetting.Controls.Add(this.label5);
+            this.grSetting.Controls.Add(this.cbPortBaud);
+            this.grSetting.Controls.Add(this.label4);
+            this.grSetting.Controls.Add(this.label3);
+            this.grSetting.Controls.Add(this.cbPortName);
+            this.grSetting.Controls.Add(this.label2);
+            this.grSetting.Controls.Add(this.label1);
+            this.grSetting.Location = new System.Drawing.Point(16, 17);
+            this.grSetting.Name = "grSetting";
+            this.grSetting.Size = new System.Drawing.Size(442, 407);
+            this.grSetting.TabIndex = 0;
+            this.grSetting.TabStop = false;
+            this.grSetting.Text = "Setting COM";
+            // 
+            // btDisconnet
+            // 
+            this.btDisconnet.Location = new System.Drawing.Point(138, 352);
+            this.btDisconnet.Name = "btDisconnet";
+            this.btDisconnet.Size = new System.Drawing.Size(276, 49);
+            this.btDisconnet.TabIndex = 3;
+            this.btDisconnet.Text = "Disconnet";
+            this.btDisconnet.UseVisualStyleBackColor = true;
+            this.btDisconnet.Click += new System.EventHandler(this.btDisconnet_Click);
+            // 
+            // btConnect
+            // 
+            this.btConnect.Location = new System.Drawing.Point(138, 297);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(276, 49);
+            this.btConnect.TabIndex = 3;
+            this.btConnect.Text = "Connect";
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // cbStopBits
+            // 
+            this.cbStopBits.FormattingEnabled = true;
+            this.cbStopBits.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.cbStopBits.Location = new System.Drawing.Point(138, 233);
+            this.cbStopBits.Name = "cbStopBits";
+            this.cbStopBits.Size = new System.Drawing.Size(276, 33);
+            this.cbStopBits.TabIndex = 4;
+            // 
+            // cbParity
+            // 
+            this.cbParity.FormattingEnabled = true;
+            this.cbParity.Items.AddRange(new object[] {
+            "none",
+            "even",
+            "odd"});
+            this.cbParity.Location = new System.Drawing.Point(138, 194);
+            this.cbParity.Name = "cbParity";
+            this.cbParity.Size = new System.Drawing.Size(276, 33);
+            this.cbParity.TabIndex = 3;
+            // 
+            // cbDataSize
+            // 
+            this.cbDataSize.FormattingEnabled = true;
+            this.cbDataSize.Items.AddRange(new object[] {
+            "7",
+            "8"});
+            this.cbDataSize.Location = new System.Drawing.Point(138, 139);
+            this.cbDataSize.Name = "cbDataSize";
+            this.cbDataSize.Size = new System.Drawing.Size(276, 33);
+            this.cbDataSize.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Stop bit:";
+            // 
+            // cbPortBaud
+            // 
+            this.cbPortBaud.FormattingEnabled = true;
+            this.cbPortBaud.Items.AddRange(new object[] {
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "38400",
+            "56000",
+            "57600",
+            "115200"});
+            this.cbPortBaud.Location = new System.Drawing.Point(138, 87);
+            this.cbPortBaud.Name = "cbPortBaud";
+            this.cbPortBaud.Size = new System.Drawing.Size(276, 33);
+            this.cbPortBaud.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Parity:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Data size:";
+            // 
+            // cbPortName
+            // 
+            this.cbPortName.FormattingEnabled = true;
+            this.cbPortName.Location = new System.Drawing.Point(138, 38);
+            this.cbPortName.Name = "cbPortName";
+            this.cbPortName.Size = new System.Drawing.Size(276, 33);
+            this.cbPortName.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Baud:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // grStatus
+            // 
+            this.grStatus.Controls.Add(this.groupBox3);
+            this.grStatus.Location = new System.Drawing.Point(495, 17);
+            this.grStatus.Name = "grStatus";
+            this.grStatus.Size = new System.Drawing.Size(630, 596);
+            this.grStatus.TabIndex = 1;
+            this.grStatus.TabStop = false;
+            this.grStatus.Text = "Status";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btLed1Off);
+            this.groupBox3.Controls.Add(this.btLed1On);
+            this.groupBox3.Location = new System.Drawing.Point(20, 40);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(170, 132);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Led 1";
+            // 
+            // btLed1Off
+            // 
+            this.btLed1Off.Location = new System.Drawing.Point(14, 87);
+            this.btLed1Off.Name = "btLed1Off";
+            this.btLed1Off.Size = new System.Drawing.Size(144, 39);
+            this.btLed1Off.TabIndex = 0;
+            this.btLed1Off.Text = "OFF";
+            this.btLed1Off.UseVisualStyleBackColor = true;
+            // 
+            // btLed1On
+            // 
+            this.btLed1On.Location = new System.Drawing.Point(14, 35);
+            this.btLed1On.Name = "btLed1On";
+            this.btLed1On.Size = new System.Drawing.Size(144, 39);
+            this.btLed1On.TabIndex = 0;
+            this.btLed1On.Text = "ON";
+            this.btLed1On.UseVisualStyleBackColor = true;
+            this.btLed1On.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btLed1On_MouseClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbMessage);
+            this.groupBox2.Location = new System.Drawing.Point(16, 430);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(442, 183);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Message";
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(7, 44);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.tbMessage.Size = new System.Drawing.Size(429, 125);
+            this.tbMessage.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1165, 645);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.grStatus);
+            this.Controls.Add(this.grSetting);
+            this.Name = "Form1";
+            this.Text = "Control led";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grSetting.ResumeLayout(false);
+            this.grSetting.PerformLayout();
+            this.grStatus.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox grSetting;
+        private System.Windows.Forms.Button btDisconnet;
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.ComboBox cbParity;
+        private System.Windows.Forms.ComboBox cbDataSize;
+        private System.Windows.Forms.ComboBox cbPortBaud;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbPortName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grStatus;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.ComboBox cbStopBits;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btLed1Off;
+        private System.Windows.Forms.Button btLed1On;
+    }
+}
+
---
+++ ControlLedApp/ControlLed/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlLed
+{
+    public partial class Form1 : Form
+    {
+        public static byte[] data = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        byte[] buf = new byte[] { 7 };
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] port = SerialPort.GetPortNames();
+            cbPortName.Items.AddRange(port);
+            btDisconnet.Enabled = false;
+        }
+
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(cbPortName.Text)
+                || string.IsNullOrEmpty(cbPortBaud.Text)
+                || string.IsNullOrEmpty(cbParity.Text)
+                || string.IsNullOrEmpty(cbDataSize.Text)
+                || string.IsNullOrEmpty(cbStopBits.Text)
+                )
+            {
+                tbMessage.Text += "Please complete setting!" + Environment.NewLine;
+            }
+            else
+            {
+                serialPort1.PortName = cbPortName.SelectedItem.ToString();
+                serialPort1.BaudRate = int.Parse(cbPortBaud.SelectedItem.ToString());
+                serialPort1.Parity = (Parity)Enum.Parse(typeof(Parity), cbParity.SelectedItem.ToString(), true);
+                serialPort1.DataBits = int.Parse(cbDataSize.SelectedItem.ToString());
+                serialPort1.StopBits = (StopBits)Enum.Parse(typeof(StopBits), cbStopBits.SelectedItem.ToString(), true);
+
+                // Set the read/write timeouts
+                serialPort1.ReadTimeout = 1500;
+                serialPort1.WriteTimeout = 500;
+
+                try
+                {
+                    serialPort1.Open();
+                    if (serialPort1.IsOpen)
+                    {
+                        tbMessage.Text += "Open " + cbPortName.SelectedItem.ToString() + Environment.NewLine;
+                        cbPortName.Enabled = false;
+                        cbPortBaud.Enabled = false;
+                        cbDataSize.Enabled = false;
+                        cbParity.Enabled = false;
+                        cbStopBits.Enabled = false;
+                        btDisconnet.Enabled = true;
+                        btConnect.Enabled = false;
+                    }
+                }
+                catch (IOException ioe)
+                {
+                    tbMessage.Text += "Error " + ioe.ToString() + Environment.NewLine;
+                }
+            }
+        }
+
+        private void btDisconnet_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            tbMessage.Text += "Close " + cbPortName.SelectedItem.ToString() + Environment.NewLine;
+            cbPortName.Enabled = true;
+            cbPortBaud.Enabled = true;
+            cbDataSize.Enabled = true;
+            cbParity.Enabled = true;
+            cbStopBits.Enabled = true;
+            btConnect.Enabled = true;
+            btDisconnet.Enabled = false;
+        }
+
+        private void btLed1On_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Write(data, 0, 1);
+                }
+                catch (TimeoutException)
+                {
+                    tbMessage.Text += "Write timeout" + Environment.NewLine;
+                }
+
+                try
+                {
+                    //serialPort1.Read(buf, 0, 1);
+                    var receive1 = serialPort1.ReadByte();
+                    string receive = serialPort1.ReadExisting();
+                    tbMessage.Text += "Receive " + receive + Environment.NewLine;
+                }
+                catch (TimeoutException)
+                {
+                    tbMessage.Text += "Read timeout value = " + buf[0]    + Environment.NewLine;
+                }
+            }
+            else
+            {
+                tbMessage.Text += "Com isn't connecting" + Environment.NewLine;
+            }
+        }
+
+    }
+}
